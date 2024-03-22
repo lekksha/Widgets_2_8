a3da241e004c11a55f876b432de990ae0340de2c
lekksha_ <lekksha@gmail.com>
2024-03-23 00:04:51 +0700

    Cosmetic changes

--- win.h
+++ win.h
@@ -3,19 +3,18 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QPushButton>
-#include <QtGui>
 
 class Counter : public QLineEdit {
     Q_OBJECT
 public:
-    Counter(const QString & contents, QWidget *parent=0) : QLineEdit(contents,parent) {}
+    Counter(const QString & contents, QWidget *parent=0) : QLineEdit(contents, parent) {}
 signals:
     void tick_signal();
 public slots:
     void add_one() {
-        QString str=text();
+        QString str = text();
         int r = str.toInt();
-        if (r!=0 && r%5 ==0)
+        if (r != 0 && r % 5 ==0)
             emit tick_signal();
         r++;
         str.setNum(r);
